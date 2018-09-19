feade1fa0680058fc2887c7cb7c06941ed743bef
zhaoyuy2 <zhaoyuy2@illinois.edu>
2018-09-19 17:39:35 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,6 @@ public class Main {
     }
     public static void main(String[] args) {
 	// write your code here
-        System.out.println();
+        System.out.println("http://erdani.com/tdpl/hamlet.txt");
     }
 }
